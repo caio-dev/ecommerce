18d6cdc8aa3c88804cdfd50104eee7c837d2bf53
Caio Cesar <caio.cesar.dec15@gmail.com>
2020-05-18 11:02:44 -0300

    Criação do login e logout

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
@@ -22,7 +24,7 @@ class Page{
 		$config = array(
 			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-			"debug"         => false // set to false to improve the speed
+			"debug"         => false
 		);
 
 		Tpl::configure( $config );
@@ -31,7 +33,7 @@ class Page{
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -57,12 +59,10 @@ class Page{
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true) $this->tpl->draw("footer");
 
 
 	}
-
-
 
 }
 
