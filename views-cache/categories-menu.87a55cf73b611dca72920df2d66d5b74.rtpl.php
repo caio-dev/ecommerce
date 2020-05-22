5fc8f53f8012ac7c5d10fb071cb3a983593ca5d2
Caio Cesar <caio.cesar.dec15@gmail.com>
2020-05-22 09:01:54 -0300

    Relacionamento de categoria e produto

--- views-cache/categories-menu.87a55cf73b611dca72920df2d66d5b74.rtpl.php
+++ views-cache/categories-menu.87a55cf73b611dca72920df2d66d5b74.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/1">Google</a></li><li><a href="/categories/8">Ferramentas</a></li><li><a href="/categories/9">Apple</a></li><li><a href="/categories/10">Microsoft</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/9">Apple</a></li><li><a href="/categories/11">Samsung</a></li><li><a href="/categories/12">Motorola</a></li>
