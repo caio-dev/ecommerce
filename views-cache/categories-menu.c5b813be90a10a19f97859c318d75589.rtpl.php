a0dc3cd3ba55c41b6e5cd05fc94063f11de4b656
Caio Cesar <caio.cesar.dec15@gmail.com>
2020-06-03 13:40:16 -0300

    Paginação e busca nos pedidos

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/1">Celulares</a></li><li><a href="/categories/2">Notebooks</a></li><li><a href="/categories/3">Televisores</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/1">Celulares</a></li><li><a href="/categories/2">Notebooks</a></li><li><a href="/categories/3">Televisores</a></li><li><a href="/categories/4">Tablets</a></li>
